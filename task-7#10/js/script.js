839a8ebb0adb3352379f2411697497f9c17986b2
Negan-ai <yoosyoos32@gmail.come>
2021-12-19 09:19:50 +0300

    7#10

---
+++ task-7#10/js/script.js
@@ -0,0 +1,53 @@
+/* slider */
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+});
+
+/* work-tabs */
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.work__btn').forEach(function (e) {
+    e.addEventListener('click', function (e) {
+      const tab = e.currentTarget.dataset.path;
+      document.querySelectorAll('.tab-content').forEach(function (e) {
+        e.classList.remove('tab-content--active')
+        document.querySelector(`[data-target='${tab}']`).classList.add('tab-content--active');
+      });
+    });
+
+    e.addEventListener('click', function (e) {
+      const tabDefault = e.currentTarget.dataset.default;
+      document.querySelectorAll('.work__btn').forEach(function (e) {
+        e.classList.remove('work__btn--default')
+        document.querySelector(`[data-path='${tabDefault}']`).classList.add('work__btn--default');
+      });
+    });
+
+  });
+})
+
+// accordion
+$( function() {
+  $( "#accordion" ).accordion({
+    collapsible: true,
+    heightStyle: 'content',
+  });
+});
+
+//bruger
+const burgerBtn = document.querySelector('.burger');
+const menuClose = document.querySelector('.menu-close');
+const menuBurger = document.querySelector('.nav');
+
+burgerBtn.addEventListener('click', () => {
+  menuBurger.classList.add('burger-active');
+});
+
+menuClose.addEventListener('click', () => {
+  menuBurger.classList.remove('burger-active');
+});
+
+
+
